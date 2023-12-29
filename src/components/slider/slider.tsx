e2d378d76d880678dfea9b47e2427bce3abe48b7
Alex Kio <alexandermartelkio@gmail.com>
2023-12-28 19:24:22 -0500

    Add recomendations

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -38,64 +38,93 @@ export default function Carousel() {
         <Slider {...settings} className={styles.headingColumn}>
           <article>
             <p>
-              Molestiae incidunt consequatur quis ipsa autem nam sit enim magni.
-              Voluptas tempore rem. Explicabo a quaerat sint autem dolore
-              ducimus ut consequatur neque. Nisi dolores quaerat fuga rem nihil
-              nostrum. Laudantium quia consequatur molestias delectus culpa.
+              We have worked on group projects together, where his strengths
+              that stood out were empathy, hard work, reliability, explaining,
+              and problem-solving. Alex is a bright, creative, detail-oriented
+              person, for whom I have great respect when it comes to both
+              personality and technical skills. Any workplace would be lucky to
+              have him on their team.
             </p>
             <div className={styles.info}>
               <Image
-                src="/assets/images/user-01.webp"
-                width={300}
-                height={300}
+                src="/assets/images/hayley.webp"
+                width={512}
+                height={512}
                 alt="Author image"
                 className={styles.avatar}
               />
               <cite className={styles.cite}>
-                <strong>Tim Cook</strong>
-                <span>CEO, Apple</span>
+                <strong>Hayley Witherell</strong>
+                <span>Software Engineer, IDBS</span>
               </cite>
             </div>
           </article>
           <article>
             <p>
-              Excepturi nam cupiditate culpa doloremque deleniti repellat.
-              Veniam quos repellat voluptas animi adipisci. Nisi eaque
-              consequatur. Voluptatem dignissimos ut ducimus accusantium
-              perspiciatis. Quasi voluptas eius distinctio. Atque eos maxime.
+              Alex was able to really quickly pick up on what I could do to make
+              my proposal successful. I left the meeting with concrete steps to
+              move forward with my proposal and knowledge of new technologies to
+              consider to make the features I was considering work! Alex has an
+              ability to actively listen, pick up on the details of your needs,
+              and articulate technical concepts very clearly. Alex is truly a
+              collaborative and empathetic teammate.
             </p>
             <div className={styles.info}>
               <Image
-                src="/assets/images/user-02.webp"
-                width={300}
-                height={300}
+                src="/assets/images/stephanie.webp"
+                width={200}
+                height={200}
                 alt="Author image"
                 className={styles.avatar}
               />
               <cite className={styles.cite}>
-                <strong>Sundar Pichai</strong>
-                <span>CEO, Google</span>
+                <strong>Stephanie Magdic</strong>
+                <span>Software Engineer, Recurly</span>
               </cite>
             </div>
           </article>
           <article>
             <p>
-              Repellat dignissimos libero. Qui sed at corrupti expedita voluptas
-              odit. Nihil ea quia nesciunt. Ducimus aut sed ipsam. Autem eaque
-              officia cum exercitationem sunt voluptatum accusamus. Quasi
-              voluptas eius distinctio. Voluptatem dignissimos ut.
+              He{"'"}s been reliable, knowledgeable, enthusiastic, and kind in
+              all my interactions with him. Even when he had a full plate with
+              his own studies, he could be counted on to reply quickly and
+              thoroughly to any message or question sent his way. Not only would
+              he provide excellent, understandable explanations of concepts, he
+              would also follow up to ensure understanding or to answer any
+              other questions that might have arisen.
             </p>
             <div className={styles.info}>
               <Image
-                src="/assets/images/user-03.webp"
-                width={300}
-                height={300}
+                src="/assets/images/megan.webp"
+                width={512}
+                height={512}
                 alt="Author image"
                 className={styles.avatar}
               />
               <cite className={styles.cite}>
-                <strong>Satya Nadella</strong>
-                <span>CEO, Microsoft</span>
+                <strong>Megan McBride</strong>
+                <span>Software Developer, TOFWERK</span>
+              </cite>
+            </div>
+          </article>
+          <article>
+            <p>
+              He is a natural-born leader that guides with compassion and
+              kindness. Whenever there is a question asked or help requested in
+              our support slack channel, Alex is consistently the first person
+              to respond and offer assistance.
+            </p>
+            <div className={styles.info}>
+              <Image
+                src="/assets/images/nina.webp"
+                width={200}
+                height={200}
+                alt="Author image"
+                className={styles.avatar}
+              />
+              <cite className={styles.cite}>
+                <strong>Nina Brissey</strong>
+                <span>Software Developer, Thoughtworks</span>
               </cite>
             </div>
           </article>
